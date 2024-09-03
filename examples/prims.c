7260aa29419c1e775629057510db9d1a1689d13c
Mayank <mayank290404gupta@gmail.com>
2024-09-03 14:38:55 +0530

    Example Updated

--- examples/prims.c
+++ examples/prims.c
@@ -6,7 +6,6 @@
 int edgeFinder(const void* node){
     return *(int*)((char*)node);  
 }
-
 int weightFinder(const void* node){
     return *(int*)((char*)node + sizeof(int));  
 }
@@ -17,14 +16,15 @@ int compare(const void* node1,const void* node2){
 }
 
 int main() {
-
+    // example test case visual https://static.javatpoint.com/ds/images/prims-algorithm.png
+    // Sample test Case's output -> ans=10
+    
     int d13[2]={3,3}; int d31[2]={1,3};
     int d32[2]={2,10}; int d23[2]={3,10};
     int d34[2]={4,2}; int d43[2]={3,2};
     int d35[2]={5,6}; int d53[2]={3,6};
     int d24[2]={4,4}; int d42[2]={2,4};
     int d45[2]={5,1}; int d54[2]={4,1};
-    // Sample test Case's output -> ans=10
     Graph* graph = createGraph(6, sizeof(d13),edgeFinder,weightFinder);
 
     addEdge(graph, 1, &d13); addEdge(graph, 3, &d31); 
@@ -49,5 +49,3 @@ int main() {
     
     return 0;
 }
-
-// example test case visual https://static.javatpoint.com/ds/images/prims-algorithm.png
